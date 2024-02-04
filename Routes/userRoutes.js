cda9a59acf55b1e46bc8b6c5d70c2092f335e05b
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2024-02-04 14:35:15 +0530

    bcrypt added

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,35 +1,48 @@
 const Users = require("../Model/Users");
 const Wallet = require("../Model/Wallet");
 const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 
 // creates new user profile
 exports.CreateUser = async (req, res) => {
-  const NEW_USER = {
-    _id: req.body._id,
-    fname: req.body.fname,
-    lname: req.body.lname,
-    email: req.body.email,
-    password: req.body.password,
-    status: "Active",
-  };
+  const checkUser = await Users.findOne({ email: req.body.email });
 
-  const user = await Users.create(NEW_USER);
+  if (!checkUser) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  const uID = String(req.body._id);
+    const NEW_USER = {
+      _id: req.body._id,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      password: hashedPassword,
+      status: "Active",
+    };
 
-  // Creating New wallet for new user with 0 balance
-  await Wallet.create({
-    _id: uID,
-    balance: 0,
-    spending: 0,
-    lastTransaction: 0,
-    lastTransactionDate: "",
-    walletstatus: "Active",
-  });
+    const user = await Users.create(NEW_USER);
 
-  res.status(201).json({
-    success: true,
-  });
+    const uID = String(req.body._id);
+
+    // Creating New wallet for new user with 0 balance
+    await Wallet.create({
+      _id: uID,
+      balance: 0,
+      spending: 0,
+      lastTransaction: 0,
+      lastTransactionDate: "",
+      walletstatus: "Active",
+    });
+
+    res.status(201).json({
+      success: true,
+    });
+  }
+  else{
+    res.status(201).json({
+      success: false,
+    });
+  }
 };
 
 // fetch user's profile details by email and password for login
@@ -65,41 +78,40 @@ exports.generateOTP = async (req, res) => {
   otp = otp.slice(7);
 
   if (account) {
-    
-  // send OTP to email
-  const mail = `
+    // send OTP to email
+    const mail = `
   <center>
     <span>Your OTP for rassword reset is </span>
     <h2>${otp}</h2>
   </center>
   `;
 
-  //sending mail
+    //sending mail
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SENDER_EMAIL,
-      pass: process.env.SENDER_PASSW,
-    },
-  });
-
-  const message = {
-    from: process.env.SENDER_EMAIL,
-    to: `${email}`,
-    subject: "OTP for Password Reset",
-    text: "Simple text",
-    html: mail,
-  };
-
-  transporter
-    .sendMail(message)
-    .then(() => {
-      console.log("mail sent scucessfully");
-    })
-    .catch(() => {
-      console.log("Error in sending mail");
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSW,
+      },
     });
+
+    const message = {
+      from: process.env.SENDER_EMAIL,
+      to: `${email}`,
+      subject: "OTP for Password Reset",
+      text: "Simple text",
+      html: mail,
+    };
+
+    transporter
+      .sendMail(message)
+      .then(() => {
+        console.log("mail sent scucessfully");
+      })
+      .catch(() => {
+        console.log("Error in sending mail");
+      });
 
     res.json({
       status: "success",
@@ -115,13 +127,15 @@ exports.generateOTP = async (req, res) => {
 // reset password of user
 exports.resetPassword = async (req, res) => {
   let { email, password } = req.body;
-  let result = await Users.findOneAndUpdate({email:email}, {password:password});
-  if(result){
+  let result = await Users.findOneAndUpdate(
+    { email: email },
+    { password: password }
+  );
+  if (result) {
     res.json({
       status: "success",
     });
-  }
-  else{
+  } else {
     res.json({
       status: "unsuccessful",
     });
